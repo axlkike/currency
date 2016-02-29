bf8fd751008ba4e4d3b5e5e3b04ac0abb988de7f
axlkike <kikejf@gmail.com>
2016-02-29 09:37:00 +0000

    first version

--- src/test/java/com/axlkike/currency/service/CurrencySearchServiceTests.java
+++ src/test/java/com/axlkike/currency/service/CurrencySearchServiceTests.java
@@ -8,7 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
